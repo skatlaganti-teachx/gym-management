2816ed1c369c2ad79a7f31bc60a1ee7d6667016b
Shuja Ali <shujaali.kunji@gmail.com>
2025-01-03 13:57:03 +0500

    updated stats icons

--- client/src/components/custom/Stats/index.tsx
+++ client/src/components/custom/Stats/index.tsx
@@ -1,6 +1,8 @@
 import { PiUsersFour } from "react-icons/pi"
 import Stat from "./Stat"
 import { StatsType } from "@/types/types"
+import { MdMoneyOffCsred, MdOutlinePaid } from "react-icons/md"
+import { LuUserRoundCheck } from "react-icons/lu"
 
 const Stats = ({
     stats
@@ -9,9 +11,9 @@ const Stats = ({
 }) => {
     return <div className="flex justify-center items-center w-full gap-7">
         <Stat title={"Total Members"} Icon={PiUsersFour} value={stats.totalMembers} />
-        <Stat title={"Attending Members"} Icon={PiUsersFour} value={stats.attendingMembers} />
-        <Stat title={"Paid Members"} Icon={PiUsersFour} value={stats.paidMembers} />
-        <Stat title={"Unpaid Members"} Icon={PiUsersFour} value={stats.unPaidMembers} />
+        <Stat title={"Attending Members"} Icon={LuUserRoundCheck} value={stats.attendingMembers} />
+        <Stat title={"Paid Members"} Icon={MdOutlinePaid} value={stats.paidMembers} />
+        <Stat title={"Unpaid Members"} Icon={MdMoneyOffCsred} value={stats.unPaidMembers} />
     </div>
 }
 
